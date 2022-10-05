ce162df52f871585f49c7620200f6df7365ead56
RikoF1 <rikof10@gmail.com>
2022-10-05 16:18:41 +0100

    first commit and updated some assets

---
+++ view/js/script.js
@@ -0,0 +1,117 @@
+var lnStickyNavigation;
+
+$(document).ready(function()
+{	
+	applyHeader();
+	applyNavigation(); 
+	applyMailTo();
+	applyResize();
+	checkHash();
+	checkBrowser();
+});
+
+/* HEADER FUNCTIONS */
+
+function applyHeader()
+{
+	$('.jumbotron').css({ height: ($(window).height()) +'px' });
+	
+	lazyLoad($('.jumbotron'));
+}	
+
+function lazyLoad(poContainer)
+{
+	
+}
+
+/* NAVIGATION FUNCTIONS */
+
+function applyNavigation()
+{
+	applyClickEvent();
+	applyNavigationFixForPhone();
+	applyScrollSpy();
+	applyStickyNavigation();
+}
+
+function applyClickEvent()
+{
+	$('a[href*=#]').on('click', function(e)
+	{
+		e.preventDefault();
+		
+		if( $( $.attr(this, 'href') ).length > 0 )
+		{
+			$('html, body').animate(
+			{
+				scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 400);
+		}
+		return false;
+	});
+}
+
+function applyNavigationFixForPhone()
+{
+	$('.navbar li a').click(function(event) 
+	{
+		$('.navbar-collapse').removeClass('in').addClass('collapse');
+	});
+}
+
+function applyScrollSpy()
+{
+	$('#navbar-example').on('activate.bs.scrollspy', function() 
+	{
+		window.location.hash = $('.nav .active a').attr('href').replace('#', '#/');
+	});
+}
+
+function applyStickyNavigation()
+{
+	lnStickyNavigation = $('.scroll-down').offset().top + 20;
+	
+	$(window).on('scroll', function() 
+	{  
+		stickyNavigation();  
+	});  
+	
+	stickyNavigation();
+}
+
+function stickyNavigation()
+{         
+	if($(window).scrollTop() > lnStickyNavigation) 
+	{   
+		$('body').addClass('fixed');  
+	} 
+	else 
+	{  
+		$('body').removeClass('fixed');   
+	}  
+}
+
+
+/* RESIZE FUNCTION */
+
+function applyResize()
+{
+	$(window).on('resize', function() 
+	{  
+		lnStickyNavigation = $('.scroll-down').offset().top + 20;
+	
+		$('.jumbotron').css({ height: ($(window).height()) +'px' });
+	}); 
+}
+
+/* HASH FUNCTION */
+
+function checkHash()
+{
+	lstrHash = window.location.hash.replace('#/', '#');
+	
+	if($('a[href='+ lstrHash +']').length > 0)
+	{
+		$('a[href='+ lstrHash +']').trigger('click');
+	}
+}
